0847584c056e7ed558c198217afe435fb559b42d
shakil89427 <shakilahmed89427@gmail.com>
2023-01-19 12:58:33 +0600

    footer url changed

--- client/src/components/global/Footer.jsx
+++ client/src/components/global/Footer.jsx
@@ -16,13 +16,17 @@ const Footer = () => {
     <footer className="bg-black text-white py-20">
       <div className="container flex flex-col gap-10 md:flex-row md:gap-20 lg:gap-32 xl:gap-40 items-center justify-center">
         <div>
-          <img
-            src={logowhite}
-            alt=""
-            className="w-[130px] md:w-[145px] lg:w-[160] xl:w-[175px] cursor-pointer block mx-auto md:mx-0"
-            onClick={() => navigate("/")}
-          />
-          <p className="text-sm">©All Right Reserved</p>
+          <a href="https://flytant.com">
+            <img
+              src={logowhite}
+              alt=""
+              className="w-[130px] md:w-[145px] lg:w-[160] xl:w-[175px] cursor-pointer block mx-auto md:mx-0"
+              onClick={() => navigate("/")}
+            />
+          </a>
+          <a href="https://flytant.com">
+            <p className="text-sm">©All Right Reserved</p>
+          </a>
         </div>
         <div className="grid grid-cols-2 gap-x-10 gap-y-3 font-medium lg:text-lg">
           {routes.map((route) => (
