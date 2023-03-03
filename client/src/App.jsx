5b84df2d4bc24763c1510b3f5d61155bf8756c06
shakil89427 <shakilahmed89427@gmail.com>
2023-03-03 16:04:18 +0600

    fallback to old content

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,30 @@
 import { Routes, Route } from "react-router-dom";
 import MainWrapper from "./components/global/MainWrapper";
 import SecondaryWrapper from "./components/global/SecondaryWrapper";
-// import Home from "./pages/Home";
-// import News from "./pages/News";
-// import Stories from "./pages/Stories";
-// import StoryDetails from "./pages/StoryDetails";
-// import Blogs from "./pages/Blogs";
-// import BlogDetails from "./pages/BlogDetails";
+import Home from "./pages/Home";
+import News from "./pages/News";
+import Stories from "./pages/Stories";
+import StoryDetails from "./pages/StoryDetails";
+import Blogs from "./pages/Blogs";
+import BlogDetails from "./pages/BlogDetails";
 import Privacy from "./pages/Privacy";
 import Terms from "./pages/Terms";
 import NotFound from "./pages/NotFound";
 import Dating from "./pages/Dating";
-import Landing1 from "./pages/Landing1";
+// import Landing1 from "./pages/Landing1";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Landing1 />} />
-      {/* <Route
+      <Route
+        path="/"
+        element={
+          <MainWrapper>
+            <Home />
+          </MainWrapper>
+        }
+      />
+      <Route
         path="/news"
         element={
           <MainWrapper>
@@ -56,7 +63,7 @@ const App = () => {
             <BlogDetails />
           </MainWrapper>
         }
-      /> */}
+      />
       <Route
         path="/privacy"
         element={
@@ -73,7 +80,7 @@ const App = () => {
           </SecondaryWrapper>
         }
       />
-      {/* <Route path="/dating" element={<Dating />} />
+      <Route path="/dating" element={<Dating />} />
       <Route
         path="/dating/privacy"
         element={
@@ -89,7 +96,7 @@ const App = () => {
             <Terms />
           </SecondaryWrapper>
         }
-      /> */}
+      />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
